e270b370f8f0dab37f4d4661ca14349f73147126
RoughRav <165179099+RoughRav@users.noreply.github.com>
2024-06-25 20:42:09 +0500

    Delete PY_home_7 directory

--- PY_home_7/import_info.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-def create_data(text: str):
-    """Добавление нового контакта"""
-    text = list(text.split())
-    with open('data_base.csv', 'a', newline= '', encoding='UTF-8') as file:
-        data = csv.writer(file)
-        data.writerow(text)
-        print('Информация внесена')
